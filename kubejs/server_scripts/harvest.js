eb41c09759c6c48a33f4912d79e994d812ba7e5c
ChiefArug <73862885+ChiefArug@users.noreply.github.com>
2022-05-14 12:53:30 +1200

    Fix erroring when right clicking stem blocks
    Also make all java imports const, and rename Block to BLOCK to prevent conflicts with the Block wrapper.

--- kubejs/server_scripts/harvest.js
+++ kubejs/server_scripts/harvest.js
@@ -1,14 +1,16 @@
 // courtesy of ChiefArug and KubeJS Discord
-let Block = java('net.minecraft.world.level.block.Block')
+const BLOCK = java('net.minecraft.world.level.block.Block')
+const STEM = java('net.minecraft.world.level.block.StemBlock')
 onEvent('block.right_click', event => {
     let block = event.block
-    if (block.hasTag('minecraft:crops') || block.hasTag('mysticalagriculture:crops')) {
+
+    if (block.hasTag('minecraft:crops')) {
         let mcLevel = event.level.minecraftLevel
         let blockState = mcLevel.getBlockState(block.pos)
         let mcBlock = blockState.block
         let mcPlayer = event.player.minecraftPlayer
-        if (mcBlock.isMaxAge(blockState)) {
-            let loot = Block.getDrops(blockState, mcLevel, block.pos, null, mcPlayer, mcPlayer.getMainHandItem())
+        if (!(mcBlock instanceof STEM) && mcBlock.isMaxAge(blockState)) {
+            let loot = BLOCK.getDrops(blockState, mcLevel, block.pos, null, mcPlayer, mcPlayer.getMainHandItem())
             let seedYeeted = false
             for (let i in loot) {
                 if (loot[i].asKJS().id == mcBlock.getCloneItemStack(mcLevel, block.pos, blockState).asKJS().id) {
@@ -18,8 +20,8 @@ onEvent('block.right_click', event => {
                 }
             }
             loot.forEach(item => {
-                Block.popResource(mcLevel, block.pos, item)
-            })
+                BLOCK.popResource(mcLevel, block.pos, item)
+            }) 
             if (seedYeeted || block.hasTag('minecraft:leaves')) {
                 block.set(block.id, {age: '0'})
                 event.server.runCommandSilent(`playsound minecraft:block.crop.break block @a ${block.x} ${block.y} ${block.z}`)
